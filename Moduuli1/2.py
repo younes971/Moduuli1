ff3a97cb83dc6058dc66ef6cea1b68f44ec6c511
youne <younesb@metropolia.fi>
2024-08-22 14:20:22 +0300

    test

---
+++ Moduuli1/2.py
@@ -0,0 +1,20 @@
+print("Terve, Younes!")
+
+LUOTI_GR = 13.3
+NAULAT_GR = 32 * LUOTI_GR
+LEIVISKA_GR = 20 * NAULAT_GR
+
+leiviskät = float(input("Anna leiviskät: "))
+naulat = float(input("Anna naulat: "))
+luodit = float(input("Anna luodit: "))
+
+grammat_leiviskät = leiviskät * LEIVISKA_GR
+grammat_naulat = naulat * NAULAT_GR
+grammat_luodit = luodit * LUOTI_GR
+
+kaikkigarmmat = int(kaikkigrammat % 1000)
+kaikkikilot = int(kaikkikilot // 1000)
+print(f"{kilot} kilogrammaa ja {grammmat:.2f} grammaa")
+
+
+
